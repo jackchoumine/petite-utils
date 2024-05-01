0f19cbd519ceb4e50c5f9831c25a863490cf6bdd
jack <jackzhoumine@gmail.com>
2024-05-01 20:19:29 +0800

    feat: 数组去重

--- src/removeRepeat.test.ts
+++ src/removeRepeat.test.ts
@@ -4,6 +4,11 @@ describe('removeRepeat', () => {
   it('should remove repeat elements', () => {
     expect(removeRepeat([1, 2, 3, 4, 5, 1, 2, 3])).toEqual([1, 2, 3, 4, 5])
   })
+  it('should remove repeat elements obj', () => {
+    expect(
+      removeRepeat([1, 2, 3, 4, 5, 1, 2, 3, {}, { 1: 3 }, { 1: 3 }]),
+    ).toEqual([1, 2, 3, 4, 5, {}, { 1: 3 }, { 1: 3 }])
+  })
 
   it('should return empty array when input is not an array', () => {
     // @ts-ignore

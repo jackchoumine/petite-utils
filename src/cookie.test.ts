7d202bee10ad09d2c7d093cdd9059c73c3764bd2
jack <jackzhoumine@gmail.com>
2024-05-01 23:57:08 +0800

    feat: set cookie

--- src/cookie.test.ts
+++ src/cookie.test.ts
@@ -1,57 +1,83 @@
-import { clearAllCookie, getCookie, removeCookie, setCookie } from './cookie'
+import { clearAllCookie, getCookie, removeCookie, setCookie } from './cookie';
+
+
+describe('removeCookie', () => {
+  beforeEach(() => {
+    document.cookie = 'name=cookie'
+  })
+  it('should remove cookie', () => {
+    removeCookie('name')
+    expect(getCookie('name')).toBe('')
+  })
+})
+
+describe('clearAllCookie', () => {
+  beforeEach(() => {
+    document.cookie = 'name=cookie'
+    document.cookie = 'name1=cookie1'
+  })
+  it('should clear all cookie', () => {
+    expect(getCookie('name')).toBe('cookie')
+    expect(getCookie('name1')).toBe('cookie1')
+
+    clearAllCookie()
+
+    expect(getCookie('name')).toBe('')
+    expect(getCookie('name1')).toBe('')
+  })
+})
 
 describe('getCookie', () => {
   afterEach(() => {
     clearAllCookie()
   })
   it('should get cookie', () => {
-    document.cookie = 'name=cookie'
+    const val = JSON.stringify('cookie')
+    document.cookie = `name=${val}`
     expect(getCookie('name')).toBe('cookie')
   })
   it('should return empty string when cookie is not exist', () => {
     document.cookie = 'name=cookie'
     expect(getCookie('name')).toBe('cookie')
-    expect(getCookie('name1')).toBe(null)
+  })
+
+  it('should get cookie with document.cookie', () => {
+    document.cookie = `name=cookie`
+    expect(getCookie('name')).toBe('cookie')
   })
 })
+
 describe('setCookie', () => {
   beforeEach(() => {
     clearAllCookie()
   })
-  it('should set cookie', () => {
+  it('should set cookie with string value', () => {
     setCookie('name', 'cookie')
     expect(getCookie('name')).toBe('cookie')
   })
+
+  it('should set cookie with object value', () => {
+    setCookie('obj', { name: 'cookie' })
+    expect(getCookie('obj')).toEqual({ name: 'cookie' })
+  })
+
+  it('should set cookie with array value', () => {
+    setCookie('array', [{ name: 'cookie' }])
+    expect(getCookie('array')).toEqual([{ name: 'cookie' }])
+  })
+
+  it('should set cookie with array value', () => {
+    setCookie('function', () => {})
+    expect(getCookie('function')).toEqual('')
+  })
+
   it('should set cookie with expires', () => {
     setCookie('name', 'cookie', 1)
     expect(getCookie('name')).toBe('cookie')
   })
-})
-describe('removeCookie', () => {
-  beforeEach(() => {
-    clearAllCookie()
-  })
-  it('should remove cookie', () => {
-    setCookie('name', 'cookie')
-    removeCookie('name')
-    expect(getCookie('name')).toBe(null)
-  })
-})
 
-describe('clearAllCookie', () => {
-  beforeEach(() => {
-    clearAllCookie()
-  })
-  it('should clear all cookie', () => {
-    setCookie('name', 'cookie')
-    setCookie('name1', 'cookie1', 30)
-
-    expect(getCookie('name')).toBe('cookie')
-    expect(getCookie('name1')).toBe('cookie1')
-
-    clearAllCookie()
-
-    expect(getCookie('name')).toBe(null)
-    expect(getCookie('name1')).toBe(null)
+  it('should set cookie with ""', () => {
+    setCookie('name', '', 1)
+    expect(getCookie('name')).toBe('')
   })
 })
--- src/cookie.ts
+++ src/cookie.ts
@@ -1,8 +1,16 @@
-import { isNumber } from './type'
+import {
+  isArray,
+  isBoolean,
+  isFunction,
+  isNullOrUndefined,
+  isNumber,
+  isObject,
+  isString,
+} from './type'
 
 export function setCookie(
   name: string,
-  value: string = '',
+  value: string | object | Array<any> | boolean = '',
   expiresInDay: number = 30,
 ) {
   if (!name) return false
@@ -10,23 +18,34 @@ export function setCookie(
   const milliseconds_one_day = 24 * 60 * 60 * 1000
   const date = new Date()
   date.setTime(date.getTime() + expiresInDay * milliseconds_one_day)
+  if (isFunction(value) || isNullOrUndefined(value)) return false
+  if (isArray(value) || isObject(value) || isBoolean(value)) {
+    value = JSON.stringify(value)
+  }
+  value = encodeURIComponent(value as string)
   // write the cookie
-  document.cookie = `${name}=${value}; expires=${date.toUTCString()}`
+  document.cookie = `${encodeURIComponent(name)}=${value}; expires=${date.toUTCString()}`
   return true
 }
 
-export function getCookie(name: string): null | string {
-  if (!name) return null
-  if (!document.cookie) return null
+export function getCookie(name: string): string {
+  if (!name) return ''
+  if (!document.cookie) return ''
   const cookies = document.cookie.split(';')
   for (let i = 0; i < cookies.length; i++) {
     const cookie = cookies[i].trim()
     const [key, value] = cookie.split('=')
     if (key === name) {
-      return value
+      try {
+        const val = JSON.parse(decodeURIComponent(value))
+        return val
+      } catch (error) {
+        // maybe set cookie by other way like document.cookie = 'name=cookie'
+        return value
+      }
     }
   }
-  return null
+  return ''
 }
 
 export function removeCookie(name: string) {

98089c94c1c99a80d1315cb23629a1d13ce05a2d
ZhouQiJun <zhouqijun4job1@163.com>
2024-08-08 18:42:17 +0800

    chore: 优化配置commitlint

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -6,7 +6,8 @@
  * @Description : commitlint 配置文件
  */
 const config = require('./.cz-config.cjs')
-console.log(config)
+// console.log(config)
+const typeList = config.types.map(item => item.value)
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
@@ -14,20 +15,7 @@ module.exports = {
     'type-enum': [
       2, // 2 表示必须
       'always',
-      [
-        'feat', // 新功能 feature
-        'fix', // 修复 bug
-        'format', // 代码格式(不影响代码运行的变动)
-        'refactor', // 重构(既不增加新功能，也不是修复bug)
-        'perf', // 性能优化的改动
-        'test', // 增加测试
-        'merge', // 合并分支
-        'docs', // 更新文档
-        'revert', // git 版本回退
-        'build', // 构建相关
-        'chore', // 其他改动
-        'release', // 发布版本
-      ],
+      typeList,
     ],
     // subject 大小写不做校验
     'subject-case': [0],

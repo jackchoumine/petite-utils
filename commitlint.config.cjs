6ff7f9ea56015c319f51e7e45d07166d4c2cf403
ZhouQiJun <zhouqijun4job1@163.com>
2024-08-08 18:44:54 +0800

    添加注释

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -2,11 +2,12 @@
  * @Author      : ZhouQiJun
  * @Date        : 2024-08-07 16:38:27
  * @LastEditors : ZhouQiJun
- * @LastEditTime: 2024-08-08 18:39:29
+ * @LastEditTime: 2024-08-08 18:44:39
  * @Description : commitlint 配置文件
  */
 const config = require('./.cz-config.cjs')
 // console.log(config)
+// 添加注释
 const typeList = config.types.map(item => item.value)
 module.exports = {
   extends: ['@commitlint/config-conventional'],

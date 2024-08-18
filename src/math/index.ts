226ca0bbfdd0e421c90b99aa204505c80f42dac7
ZhoQiJun <zhouqijun4job@163.com>
2024-08-18 16:58:22 +0800

    docs: 完善取模函数的注释

--- src/math/index.ts
+++ src/math/index.ts
@@ -1,14 +1,21 @@
 /**
- * m mod n
+ * m mod n,
+ * it is different from % operator in javascript
+ * % operator is remainder operator,  m % n is same to  m - n * Math.trunc(m / n).
+ * Modulo operator is m mod n is same to  m - n * Math.floor(m / n)
+ * @link https://www.designcise.com/web/tutorial/what-is-the-difference-between-the-javascript-remainder-operator-and-the-modulo-operator
  * @example
  * modulo(7, 3) // 1
- * modulo(7, -3) // 1
+ * modulo(-7, -3) // -1
+ * modulo(7, -3) // -2
  * modulo(-7, 3) // 2
  * @param m
  * @param n
  * @returns
  */
 function modulo(m: number, n: number): number {
+  // same to
+  // m - n * Math.floor(m / n)
   return ((m % n) + n) % n
 }
 

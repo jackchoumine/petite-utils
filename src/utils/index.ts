9bd9c68f204598b69a038dcc2012f4d9fc3ba3ed
ZhouQiJun <zhouqijun4job1@163.com>
2024-10-28 18:14:19 +0800

    chore: 修改文件名

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,7 +2,7 @@
  * @Author      : ZhouQiJun
  * @Date        : 2024-10-28 01:35:13
  * @LastEditors : ZhouQiJun
- * @LastEditTime: 2024-10-28 01:35:39
+ * @LastEditTime: 2024-10-28 18:13:54
  * @Description : export all utils
  */
-export * from './repeat'
+export * from './repeatRun'

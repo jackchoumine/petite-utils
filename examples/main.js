dbdf94f7285f5856099fe18acd3ea276a1ee4a3f
ZhouQiJun <zhouqijun4job1@163.com>
2024-10-28 16:40:37 +0800

    test: test 测试 repeat

--- examples/main.js
+++ examples/main.js
@@ -2,11 +2,14 @@
  * @Author      : ZhouQiJun
  * @Date        : 2024-05-28 10:20:30
  * @LastEditors : ZhouQiJun
- * @LastEditTime: 2024-08-08 19:03:54
+ * @LastEditTime: 2024-10-28 16:36:50
  * @Description : 测试 esm
  */
-import { sum } from 'petite-utils'
+import { repeat } from 'petite-utils'
 
-const result = sum(1, 20)
-console.log(result) // 3
-console.log('test in browser using vite')
+repeat((stop, repeatTimes) => {
+  console.log('repeatTimes:', repeatTimes)
+  if (repeatTimes > 5) {
+    stop()
+  }
+})

fe8b8711ce11be6907552eca8aabc411240f4227
ZhouQiJun <zhouqijun4job@163.com>
2025-03-22 20:37:12 +0800

    fix: 随机整数位数不稳定

--- src/randomNum.test.ts
+++ src/randomNum.test.ts
@@ -1,3 +1,10 @@
+/*
+ * @Author      : ZhouQiJun
+ * @Date        : 2025-03-22 19:49:08
+ * @LastEditors : ZhouQiJun
+ * @LastEditTime: 2025-03-22 20:35:28
+ * @Description : 随机数字测试
+ */
 import { describe, expect, it } from 'vitest'
 
 import { randomNum } from './randomNum'
@@ -13,7 +20,7 @@ describe('randomNum', () => {
     })
   })
   describe('传递参数', () => {
-    it('应该返回一个10位整数', () => {
+    it('应该返回一个 10 位整数', () => {
       const n = randomNum(10)
 
       expect(isNumber(n)).toBe(true)
--- src/randomNum.ts
+++ src/randomNum.ts
@@ -2,14 +2,19 @@
  * @Author      : ZhouQiJun
  * @Date        : 2025-03-06 16:02:51
  * @LastEditors : ZhouQiJun
- * @LastEditTime: 2025-03-06 16:06:55
+ * @LastEditTime: 2025-03-22 20:34:42
  * @Description : randomNum 函数 - 生成随机数
  */
-export function randomNum(length: number = 6): number {
+export function randomNum(length: number = 6) {
   const arr: number[] = []
   let n = ''
+  const str = '1234566789'
   for (var i = 0; i < length; i++) {
-    arr[i] = Number.parseInt(Math.random() * 10 + '')
+    if (i === 0) {
+      arr[i] = +str[Math.ceil(Math.random() * 9)]
+    } else {
+      arr[i] = Number.parseInt(Math.random() * 10 + '')
+    }
     n += String(arr[i])
   }
   return Number.parseInt(n)

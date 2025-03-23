abb7c8d8d18d740cc2f916b4abbc1ff63396926e
ZhouQiJun <zhouqijun4job@163.com>
2025-03-23 13:26:49 +0800

    chore: 测试覆盖率统计排除 index.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,7 +2,7 @@
  * @Author      : ZhouQiJun
  * @Date        : 2024-05-28 10:20:30
  * @LastEditors : ZhouQiJun
- * @LastEditTime: 2024-07-11 18:35:28
+ * @LastEditTime: 2025-03-23 13:19:35
  * @Description : vitest 配置文件
  */
 import AutoImport from 'unplugin-auto-import/vite'
@@ -22,7 +22,7 @@ export default defineConfig({
     coverage: {
       // exclude: ['src/**/*.test.ts']
       include: ['src/**/*.ts'],
-      exclude: ['src/index.ts'],
+      exclude: ['src/**/index.ts'],
     },
   },
 })

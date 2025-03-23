6b6d8c5696a60f75de8a0e5e8d8fb83794a228f0
ZhouQiJun <zhouqijun4job@163.com>
2025-03-23 21:09:16 +0800

    fix: 修复 throttle 立即执行 和 在间隔内，至少保证一次调用

--- src/utils/throttle.ts
+++ src/utils/throttle.ts
@@ -2,31 +2,47 @@
  * @Author      : ZhouQiJun
  * @Date        : 2024-05-28 03:04:01
  * @LastEditors : ZhouQiJun
- * @LastEditTime: 2025-03-22 22:03:58
+ * @LastEditTime: 2025-03-23 21:06:42
  * @Description : 节流
  */
 /**
  * @group 工具函数
  * @param fn 待节流的函数
- * @param wait 间隔时间，毫秒
- * @param isImmediate 是否立即执行
+ * @param interval 间隔时间，毫秒，默认 500
+ * @param immediate 是否立即执行，默认 false
  * @returns
  */
-export function throttle(fn, wait = 500, isImmediate = false) {
-  let last = Date.now()
-  let timer
+export function throttle(fn, interval = 200, immediate = false) {
+  let last = Date.now() // 上次执行的时间戳
+  let timer = null // 定时器
+  let hasCalled = false // 是否是第一次调用
+
   return (...rest) => {
-    isImmediate && fn(...rest)
     const now = Date.now()
-    clearTimeout(timer)
-    if (now - last >= wait) {
+
+    if (immediate && !hasCalled) {
+      // 如果是第一次调用且需要立即执行
       fn(...rest)
       last = now
-      return
+      hasCalled = true
     }
-    //NOTE 事件结束后，保证再执行一次
-    timer = setTimeout(() => {
+
+    clearTimeout(timer) // 清除之前的定时器
+
+    if (now - last >= interval) {
+      // 如果距离上次执行的时间超过 interval，则立即执行
       fn(...rest)
-    }, wait)
+      last = now
+    } else {
+      // 否则，设置定时器，在剩余时间后执行
+      timer = setTimeout(
+        () => {
+          fn(...rest)
+          last = Date.now()
+        },
+        // 保证时间间隔为 interval 内
+        interval - (now - last),
+      )
+    }
   }
 }
